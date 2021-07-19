8c8498dc70bae508f6940f7de65a81d285a09a47
Sammoned1 <82578998+Sammoned1@users.noreply.github.com>
2021-07-19 22:55:49 +0300

    Add files via upload

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -1,22 +1,27 @@
-
 $(document).ready(function () {
+
+
+
     $(window).scroll(() => {
         let scrollDistance = $(window).scrollTop();
         $(".section").each((i, el) => {
-            if ($(el).offset().top - $("nav").outerHeight() <= scrollDistance) {
+            if ($(el).offset().top - $("nav").outerHeight() <= scrollDistance + 100 + "px") {
                 $("nav a").each((i, el) => {
-                    if (($el).hasClass("active")){
+                    if ($(el).hasClass("active")) {
                         $(el).removeClass("active");
                     }
-                    
+
                 });
-                $("nav:eq("+ i +")").find('a').addClass('active');
+                $("nav li:eq(" + i + ")").find('a').addClass('active');
             }
+
         });
     });
 });
 
-$('a[href^="#"]').click(function(){
+$('a[href^="#"]').click(function () {
     let valHref = $(this).attr("href");
-    $('html, body').animate({scrollTop: $(valHref).offset().top - 30 + "px"});
+    $('html, body').animate({
+        scrollTop: $(valHref).offset().top - 60 + "px"
+    });
 });
